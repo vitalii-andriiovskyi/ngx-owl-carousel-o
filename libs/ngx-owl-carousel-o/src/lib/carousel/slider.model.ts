0fbb6bc8bc689e44ef3cedbd9c9b3b99f8bad012
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-07-12 21:50:04 +0300

    add data model of slider

---
+++ libs/ngx-owl-carousel-o/src/lib/carousel/slider.model.ts
@@ -0,0 +1,8 @@
+import { TemplateRef } from "../../../../../node_modules/@angular/core";
+
+export class SliderModel {
+  id: string;
+  active: boolean;
+  tplRef: TemplateRef<any>;
+  dataMerge: number;
+}
