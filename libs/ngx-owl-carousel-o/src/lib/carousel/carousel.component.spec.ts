bd10164ff8eb8ed8e250b753ce4bc701ebb45b40
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-07-17 19:01:58 +0300

    add function createTestComponent; prepare carousel.component.spec to testing

--- libs/ngx-owl-carousel-o/src/lib/carousel/carousel.component.spec.ts
+++ libs/ngx-owl-carousel-o/src/lib/carousel/carousel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, discardPeriodicTasks, TestBed } from '@angular/core/testing';
 import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
@@ -9,31 +9,12 @@ import {
 import { ResizeService } from '../services/resize.service';
 import { WINDOW_PROVIDERS } from '../services/window-ref.service';
 import { CarouselService } from '../services/carousel.service';
+import { createGenericTestComponent } from './test/common';
 
-@Component({
-  template: `
-    <div class="carousel-wrapper" style="width: 1000px;">
-      <owl-carousel-o>
-        <ng-template carouselSlide>
-          Slide 1
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 2
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 3
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 4
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 5
-        </ng-template>
-      </owl-carousel-o>
-    </div>
-  `
-})
-class TestComponent {}
+
+const createTestComponent = (html: string) =>
+    createGenericTestComponent(html, TestComponent) as ComponentFixture<TestComponent>
+
 
 describe('SurfCarousel2Component with prop "cycled=true"', () => {
   let hostComponent: TestComponent;
@@ -59,40 +40,13 @@ describe('SurfCarousel2Component with prop "cycled=true"', () => {
           CarouselSlideDirective
         ],
         providers: [ResizeService, WINDOW_PROVIDERS, CarouselService]
-      }).compileComponents();
+      });
     })
   );
 
-  beforeEach(() => {
-    fixtureHost = TestBed.createComponent(TestComponent);
-    hostComponent = fixtureHost.componentInstance;
-
-    fixtureHost.detectChanges();
-
-    deCarouselComponent = fixtureHost.debugElement.query(
-      By.css('owl-carousel-o')
-    );
-    carouselComponent = deCarouselComponent.componentInstance;
-    carouselService = deCarouselComponent.injector.get(CarouselService);
-
-    fixtureHost.detectChanges();
-
-    deNavButtons = fixtureHost.debugElement.queryAll(
-      By.css('.owl-nav > div')
-    );
-    leftButton = deNavButtons[0].nativeElement;
-    rightButton = deNavButtons[1].nativeElement;
-    deStages = fixtureHost.debugElement.queryAll(
-      By.css('.owl-stage')
-    );
-    // deSlides = fixtureHost.debugElement.queryAll(
-    //   By.css('.surf-carousel-2-slide-wrapper')
-    // );
-  });
-
-  it('should create SurfCarousel2Component', () => {
-    expect(hostComponent).toBeTruthy();
-  });
+  // it('should create SurfCarousel2Component', () => {
+  //   expect(hostComponent).toBeTruthy();
+  // });
 
 
   // it('should have 10 sliders and 2 stages whilest prop "cycled" is true', () => {
@@ -164,135 +118,6 @@ describe('SurfCarousel2Component with prop "cycled=true"', () => {
 });
 
 @Component({
-  template: `
-    <div class="carousel-wrapper" style="width: 1000px;">
-      <owl-carousel-o>
-        <ng-template carouselSlide>
-          Slide 1
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 2
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 3
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 4
-        </ng-template>
-        <ng-template carouselSlide>
-          Slide 5
-        </ng-template>
-      </owl-carousel-o>
-    </div>
-  `
+  template: ''
 })
-class TestSingleComponent {}
-
-describe('SurfCarousel2Component without prop "cycled=true"', () => {
-  let hostComponent: TestSingleComponent;
-  let fixtureHost: ComponentFixture<TestSingleComponent>;
-
-  let carouselComponent: CarouselComponent;
-  let deCarouselComponent: DebugElement;
-
-  let deNavButtons: DebugElement[];
-  let rightButton: HTMLElement;
-  let leftButton: HTMLElement;
-
-  let deSlides: DebugElement[];
-  let deStage: DebugElement;
-
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          CarouselComponent,
-          TestSingleComponent,
-          CarouselSlideDirective
-        ],
-        providers: [ResizeService, WINDOW_PROVIDERS, CarouselService]
-      }).compileComponents();
-    })
-  );
-
-  beforeEach(() => {
-    fixtureHost = TestBed.createComponent(TestSingleComponent);
-    hostComponent = fixtureHost.componentInstance;
-
-    fixtureHost.detectChanges();
-    deCarouselComponent = fixtureHost.debugElement.query(
-      By.css('owl-carousel-o')
-    );
-    fixtureHost.detectChanges();
-
-    deNavButtons = fixtureHost.debugElement.queryAll(
-      By.css('.owl-nav > div')
-    );
-    leftButton = deNavButtons[0].nativeElement;
-    rightButton = deNavButtons[1].nativeElement;
-    deStage = fixtureHost.debugElement.query(By.css('.owl-stage'));
-    // deSlides = fixtureHost.debugElement.queryAll(
-    //   By.css('.surf-carousel-2-slide-wrapper')
-    // );
-  });
-
-  // it('should have 5 sliders', () => {
-  //   expect(deSlides.length).toBe(5, 'must be 5 sliders');
-  // });
-
-  // it('should have 1 active slider', () => {
-  //   deSlides = deStage.queryAll(By.css('.active'));
-  //   expect(deSlides.length).toBe(1, '1 active slide');
-  // });
-
-  // it('first button (arrow-left) should have class .disabled', () => {
-  //   expect(leftButton.classList.contains('disabled')).toBeTruthy();
-  // });
-
-  // it('should move carousel left on 1 slide after clicking right button', () => {
-  //   deNavButtons[1].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-
-  //   deSlides = deStage.queryAll(By.css('.surf-carousel-2-slide-wrapper'));
-  //   expect(deSlides[0].nativeElement.classList.contains('active')).toBeFalsy(
-  //     "1t slide hasn't .active class"
-  //   );
-  //   expect(deSlides[1].nativeElement.classList.contains('active')).toBeTruthy(
-  //     '2d slide has .active class'
-  //   );
-  //   expect(leftButton.classList.contains('disabled')).toBeFalsy(
-  //     "left arrow hasn't .disable class"
-  //   );
-  // });
-
-  // it("shouldn't move carousel right after clicking left button while 1t slide is active", () => {
-  //   deNavButtons[0].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-  //   deSlides = deStage.queryAll(By.css('.surf-carousel-2-slide-wrapper'));
-  //   expect(deSlides[0].nativeElement.classList.contains('active')).toBeTruthy(
-  //     '1t slide hasn .active class'
-  //   );
-  //   expect(leftButton.classList.contains('disabled')).toBeTruthy(
-  //     'left arrow has .disable class'
-  //   );
-  // });
-
-  // it('should right button be disabled while last slide is active', () => {
-  //   deNavButtons[1].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-  //   deNavButtons[1].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-  //   deNavButtons[1].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-  //   deNavButtons[1].triggerEventHandler('click', null);
-  //   fixtureHost.detectChanges();
-
-  //   deSlides = deStage.queryAll(By.css('.surf-carousel-2-slide-wrapper'));
-  //   expect(rightButton.classList.contains('disabled')).toBeTruthy(
-  //     'right arrow has .disable class'
-  //   );
-  //   expect(deSlides[4].nativeElement.classList.contains('active')).toBeTruthy(
-  //     'last slide has .active class'
-  //   );
-  // });
-});
+class TestComponent {}
