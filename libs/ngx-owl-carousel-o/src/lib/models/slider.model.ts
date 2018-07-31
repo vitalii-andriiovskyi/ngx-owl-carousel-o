4d38293c33a12a5f82bffc7836586d9e4e47f748
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-07-31 18:17:51 +0300

    change the SliderModel

--- libs/ngx-owl-carousel-o/src/lib/models/slider.model.ts
+++ libs/ngx-owl-carousel-o/src/lib/models/slider.model.ts
@@ -2,12 +2,12 @@ import { TemplateRef } from "@angular/core";
 
 export class SliderModel {
   id: string;
-  active: boolean;
-  tplRef: TemplateRef<any>;
-  dataMerge: number;
-  width: number | string;
+  active?: boolean;
+  tplRef?: TemplateRef<any>;
+  dataMerge?: number;
+  width?: number | string;
   marginL?: number | string;
   marginR?: number | string;
   center?: boolean;
-  cloned: boolean;
+  cloned?: boolean;
 }
