51818881a91b09cc3f59745c4d1a1370b8f734ab
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-02 20:46:39 +0300

    add initial empty event listeners and name of methods wich will be attached to mousemove and touchmove events; DraggableDirective

--- libs/ngx-owl-carousel-o/src/lib/carousel/draggable.directive.ts
+++ libs/ngx-owl-carousel-o/src/lib/carousel/draggable.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, NgZone, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { CarouselService } from '../services/carousel.service';
 
 @Directive({
   selector: '[owlDraggable]'
 })
 export class DraggableDirective {
+  listenerMouseMove: () => void;
+  listenerTouchMove: () => void;
+  listenerOneMouseMove: () => void;
+  listenerOneTouchMove: () => void;
 
-  constructor() { }
+  constructor(private zone: NgZone,
+              private el: ElementRef,
+              private renderer: Renderer2,
+              private carouselService: CarouselService) { }
+
+  @HostListener('mousedown', ['$event']) onMouseDown(event) {
+    return false;
+  }
+
+  @HostListener('mouseup', ['$event']) onMouseUp(event) {
+    return false;
+  }
+
+  @HostListener('touchend', ['$event']) onTouchEnd(event) {
+    return false;
+  }
+
+  @HostListener('dragstart') onDragStart() {
+    return false;
+  }
+
+  @HostListener('selectstart') onSelectStart() {
+    return false;
+  }
+
 
 }
