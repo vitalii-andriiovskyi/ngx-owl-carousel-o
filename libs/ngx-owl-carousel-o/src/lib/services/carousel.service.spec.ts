dedd63dd3e5ef0bbeeb2a6af87cffcc2a68c5ec2
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-06 13:51:09 +0300

    prepare tests of CarouselService for testing in context of TestComponent and add TestComponent

--- libs/ngx-owl-carousel-o/src/lib/services/carousel.service.spec.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/carousel.service.spec.ts
@@ -1,7 +1,17 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, ComponentFixture, fakeAsync, async, tick } from '@angular/core/testing';
 
 import { CarouselService } from './carousel.service';
 import { CustomEventsService } from './custom-events.service';
+import { Component, DebugElement } from '@angular/core';
+import { SlidesOutputData, CarouselComponent, CarouselSlideDirective } from '../carousel/carousel.component';
+import { createGenericTestComponent } from '../carousel/test/common';
+import { DraggableDirective } from '../carousel/draggable.directive';
+import { ResizeService } from './resize.service';
+import { WINDOW_PROVIDERS } from './window-ref.service';
+import { NavigationService } from './navigation.service';
+
+import 'zone.js/dist/zone-patch-rxjs-fake-async';
+import { By } from '../../../../../node_modules/@angular/platform-browser';
 
 describe('CarouselService', () => {
   let carouselService: CarouselService;
@@ -49,3 +59,44 @@ describe('CarouselService', () => {
   });
 
 });
+
+const createTestComponent = (html: string) =>
+    createGenericTestComponent(html, TestComponent) as ComponentFixture<TestComponent>
+
+describe('CarouselService in context of TestComponent', () => {
+  let carouselService: CarouselService;
+  let testComponent: TestComponent;
+
+  let fixtureHost: ComponentFixture<TestComponent>;
+
+  let deCarouselComponent: DebugElement;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          CarouselComponent,
+          TestComponent,
+          CarouselSlideDirective,
+          DraggableDirective
+        ],
+        providers: [ResizeService, WINDOW_PROVIDERS, CarouselService, NavigationService]
+      });
+    })
+  );
+
+
+}
+
+@Component({
+  selector: 'test-dom',
+  template: ''
+})
+class TestComponent {
+  options: any = {};
+  translatedData: SlidesOutputData;
+  constructor() {}
+  getPassedData(data: any) {
+    this.translatedData = data;
+  }
+}
