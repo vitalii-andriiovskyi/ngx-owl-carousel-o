2bf077d2a9bba0f7c23bf545ce34fdad422ee3fc
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-08 18:35:47 +0300

    add tests for DraggableDirective; mouseDrag=false; mouse dragging

--- libs/ngx-owl-carousel-o/src/lib/carousel/draggable.directive.spec.ts
+++ libs/ngx-owl-carousel-o/src/lib/carousel/draggable.directive.spec.ts
@@ -677,6 +677,54 @@ describe('DraggableDirective in context of CarouselComponent (integrated tests)'
     expect(deDots[0].nativeElement.classList.contains('active')).toBeTruthy('1th dot is active');
     discardPeriodicTasks();
   }));
+
+  it(`shouldn\'t drag carousel by mouse [options]="{mouseDrag: false}`, fakeAsync(() => {
+    const html = `
+      <div style="width: 920px; margin: auto">
+        <owl-carousel-o [options]="{mouseDrag: false}">
+          <ng-template carouselSlide>Slide 1</ng-template>
+          <ng-template carouselSlide>Slide 2</ng-template>
+          <ng-template carouselSlide>Slide 3</ng-template>
+          <ng-template carouselSlide>Slide 4</ng-template>
+          <ng-template carouselSlide>Slide 5</ng-template>
+        </owl-carousel-o>
+      </div>
+    `;
+    fixtureHost = createTestComponent(html);
+    testComponent = fixtureHost.componentInstance;
+    deCarouselComponent = fixtureHost.debugElement.query(By.css('owl-carousel-o'));
+    tick();
+
+    fixtureHost.detectChanges();
+    deStage = deCarouselComponent.query(By.css('.owl-stage'));
+    deSlides = deCarouselComponent.queryAll(By.css('.owl-item'));
+    coords = findCoordsInElem(deSlides[1].nativeElement, getCoords(deSlides[1].nativeElement));
+
+    deDots = deCarouselComponent.queryAll(By.css('.owl-dots .owl-dot'));
+
+    expect(deDots.length).toBe(2, '2 dots');
+
+    // drag carousel to left hand-side; current first active slide is Slide 1;
+    triggerMouseEvent(deStage.nativeElement, 'mousedown', {clientX: coords.x, clientY: coords.y});
+    triggerMouseEvent(document, 'mousemove', {clientX: coords.x, clientY: coords.y});
+    tick();
+    triggerMouseEvent(document, 'mousemove', {clientX: coords.x -10, clientY: coords.y});
+    tick();
+    triggerMouseEvent(document, 'mousemove', {clientX: coords.x - 300, clientY: coords.y});
+    tick();
+    triggerMouseEvent(document, 'mouseup', {clientX: coords.x - 300, clientY: coords.y});
+    tick();
+    fixtureHost.detectChanges();
+
+    deActiveSlides = deCarouselComponent.queryAll(By.css('.owl-item.active'));
+    expect(deActiveSlides[0].nativeElement.innerHTML).toContain('Slide 1', 'Slide 1');
+    expect(deActiveSlides.length).toBe(3, '3 active slide');
+    deDots = deCarouselComponent.queryAll(By.css('.owl-dots .owl-dot'));
+    expect(deDots[0].nativeElement.classList.contains('active')).toBeTruthy('1th dot is active');
+
+    discardPeriodicTasks();
+  }));
+
   // the ending of tests
 });
 
