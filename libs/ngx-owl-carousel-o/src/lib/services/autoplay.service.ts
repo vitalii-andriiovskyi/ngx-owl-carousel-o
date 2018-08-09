f9390a620c7a0a0fb52b6cf1a28e32c3d8a4af7c
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 15:07:59 +0300

    add props _paused, _timeout and autoplaySubscription; AutoplayService

--- libs/ngx-owl-carousel-o/src/lib/services/autoplay.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/autoplay.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CarouselService } from './carousel.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutoplayService {
-  
-  constructor() { }
+  /**
+   * Subscrioption to merge Observables from CarouselService
+   */
+  autoplaySubscription: Subscription;
+
+  /**
+   * The autoplay timeout.
+   */
+  private _timeout: number = null;
+
+  /**
+   * Indicates whenever the autoplay is paused.
+   */
+  private _paused = false;
+
+  constructor(private carouselService: CarouselService) { }
 }
