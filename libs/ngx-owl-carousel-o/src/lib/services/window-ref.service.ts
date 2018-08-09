c096303dbd346adb19547c5652f899b38dbc50b4
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 14:09:32 +0300

    add jsdoc comment to WindowRef Service

--- libs/ngx-owl-carousel-o/src/lib/services/window-ref.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/window-ref.service.ts
@@ -18,28 +18,42 @@ import {
   PLATFORM_ID
 } from '@angular/core';
 
-/* Create a new injection token for injecting the window into a component. */
+/**
+ * Create a new injection token for injecting the window into a component.
+ */
 export const WINDOW = new InjectionToken('WindowToken');
 
-/* Define abstract class for obtaining reference to the global window object. */
+/**
+ * Define abstract class for obtaining reference to the global window object.
+ */
 export abstract class WindowRef {
   get nativeWindow(): Window | Object {
     throw new Error('Not implemented.');
   }
 }
 
-/* Define class that implements the abstract class and returns the native window object. */
+/**
+ * Define class that implements the abstract class and returns the native window object.
+ */
 export class BrowserWindowRef extends WindowRef {
   constructor() {
     super();
   }
 
+  /**
+   * @returns window object
+   */
   get nativeWindow(): Window | Object {
     return window;
   }
 }
 
-/* Create an factory function that returns the native window object. */
+/**
+ * Create an factory function that returns the native window object.
+ * @param browserWindowRef Native window object
+ * @param platformId id of platform
+ * @returns type of platform of empty object
+ */
 export function windowFactory(
   browserWindowRef: BrowserWindowRef,
   platformId: Object
@@ -50,18 +64,24 @@ export function windowFactory(
   return new Object();
 }
 
-/* Create a injectable provider for the WindowRef token that uses the BrowserWindowRef class. */
+/**
+ * Create a injectable provider for the WindowRef token that uses the BrowserWindowRef class.
+ */
 export const browserWindowProvider: ClassProvider = {
   provide: WindowRef,
   useClass: BrowserWindowRef
 };
 
-/* Create an injectable provider that uses the windowFactory function for returning the native window object. */
+/**
+ * Create an injectable provider that uses the windowFactory function for returning the native window object.
+ */
 export const windowProvider: FactoryProvider = {
   provide: WINDOW,
   useFactory: windowFactory,
   deps: [WindowRef, PLATFORM_ID]
 };
 
-/* Create an array of providers. */
+/**
+ * Create an array of providers.
+ */
 export const WINDOW_PROVIDERS = [browserWindowProvider, windowProvider];
