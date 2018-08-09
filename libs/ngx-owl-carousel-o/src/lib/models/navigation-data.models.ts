04eeb913a9acae34f5da38e09de175d7b315d45a
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 12:59:50 +0300

    correct jsdoc Navigation data

--- libs/ngx-owl-carousel-o/src/lib/models/navigation-data.models.ts
+++ libs/ngx-owl-carousel-o/src/lib/models/navigation-data.models.ts
@@ -1,30 +1,30 @@
 /**
- * data model for single navigation button
+ * Data model for single navigation button
  */
 export class NavButton {
 	/**
-	 * make button disabled by attaching class .disabled; it's being used when otpion loop=false
+	 * Make button disabled by attaching class .disabled; it's being used when otpion loop=false
 	 */
 	disabled: boolean;
 	/**
-	 * text for button; can be simple text or html-markup (e.g. <i class="fa fa-next">)
+	 * Text for button; can be simple text or html-markup (e.g. <i class="fa fa-next">)
 	 */
 	htmlText: string;
 }
 /**
- * data model for  navigation block
+ * Data model for  navigation block
  */
 export class NavData {
 	/**
-	 * turns on or turns off navigation block
+	 * Rurns on or turns off navigation block
 	 */
 	disabled: boolean;
 	/**
-	 * navigation button 'prev'
+	 * Navigation button 'prev'
 	 */
 	prev: NavButton;
 	/**
-	 * navigation button 'next'
+	 * Navigation button 'next'
 	 */
 	next: NavButton;
 }
@@ -33,32 +33,32 @@ export class NavData {
  */
 export class OwlSingeDot {
 	/**
-	 * id for dot button
+	 * Id for dot button
 	 */
 	id: string;
 	/**
-	 * makes dot active by attaching .active class to it
+	 * Makes dot active by attaching .active class to it
 	 */
 	active: boolean;
 	/**
-	 * inner content of dot; can be html-markup
+	 * Inner content of dot; can be html-markup
 	 */
 	innerContent?: string;
 	/**
-	 * enabled css-class which gives right presentaion of innerContent of dot.
+	 * Enabled css-class which gives right presentaion of innerContent of dot.
 	 */
 	showInnerContent?: boolean;
 }
 /**
- * data model for owl dot-block
+ * Data model for owl dot-block
  */
 export class DotsData {
 	/**
-	 * turns on or turns off navigation block
+	 * Turns on or turns off navigation block
 	 */
 	disabled: boolean;
 	/**
-	 * array of dots
+	 * Array of dots
 	 */
 	dots: OwlSingeDot[];
 }
