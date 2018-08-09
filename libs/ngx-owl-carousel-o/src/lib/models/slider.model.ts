7f934beb3b2027fb017871b916cd0d6b39da4b19
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 13:44:38 +0300

    add jsdoc to SliderModel

--- libs/ngx-owl-carousel-o/src/lib/models/slider.model.ts
+++ libs/ngx-owl-carousel-o/src/lib/models/slider.model.ts
@@ -1,13 +1,49 @@
 import { TemplateRef } from "@angular/core";
 
 export class SliderModel {
+
+  /**
+   * Id of slide
+   */
   id: string;
+
+  /**
+   * Active state of slide. If true slide gets css-class .active
+   */
   active?: boolean;
+
+  /**
+   * TemplateRef of slide. In other words its html-markup
+   */
   tplRef?: TemplateRef<any>;
+
+  /**
+   * Number of grid parts to be used
+   */
   dataMerge?: number;
+
+  /**
+   * Width of slide
+   */
   width?: number | string;
+
+  /**
+   * Css-rule 'margin-left'
+   */
   marginL?: number | string;
+
+  /**
+   * Css-rule 'margin-right'
+   */
   marginR?: number | string;
+
+  /**
+   * Make slide to be on center of the carousel
+   */
   center?: boolean;
+
+  /**
+   * Cloned slide. It's being used when 'loop'=true
+   */
   cloned?: boolean;
 }
