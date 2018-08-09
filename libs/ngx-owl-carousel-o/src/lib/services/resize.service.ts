5de0e39bd28633830907b8e7115b64b2336e5d77
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 14:04:11 +0300

    add jsdoc to ResizeService

--- libs/ngx-owl-carousel-o/src/lib/services/resize.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/resize.service.ts
@@ -4,12 +4,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ResizeService {
+  /**
+   * Width of window
+   */
   public windowWidth: any;
 
+  /**
+   * Makes resizeSubject become Observable
+   * @returns Observable of resizeSubject
+   */
   get onResize$(): Observable<Window> {
     return this.resizeSubject.asObservable();
   }
 
+  /**
+   * Subject of 'resize' event
+   */
   private resizeSubject: Subject<Window>;
 
   constructor(private eventManager: EventManager) {
@@ -26,10 +36,18 @@ export class ResizeService {
     );
   }
 
+  /**
+   * Handler of 'resize' event. Passes data throw resizeSubject
+   * @param event Event Object of 'resize' event
+   */
   private onResize(event: UIEvent) {
     this.resizeSubject.next(<Window>event.target);
   }
 
+  /**
+   * Handler of 'onload' event. Defines the width of window
+   * @param event Event Object of 'onload' event
+   */
   private onLoaded(event: UIEvent) {
     this.windowWidth = <Window>event.target;
   }
