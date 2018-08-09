7a2ad0080209919155d60a3ae15ca2c6baddecb5
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-09 15:12:30 +0300

    add method spyDataStreams(); AutoplayService

--- libs/ngx-owl-carousel-o/src/lib/services/autoplay.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/autoplay.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, merge } from 'rxjs';
 import { CarouselService } from './carousel.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +23,31 @@ export class AutoplayService {
   private _paused = false;
 
   constructor(private carouselService: CarouselService) { }
+
+  /**
+   * Defines Observables which service must observe
+   */
+  spyDataStreams() {
+
+    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState().pipe(
+      tap(state => {
+        // this.initialize();
+        this.carouselService.sendChanges();
+      })
+    );
+
+    // mostly changes in carouselService and carousel at all causes carouselService.to(). It moves stage right-left by its code and calling needed functions
+    // Thus this method by calling carouselService.current(position) notifies about changes
+    const changedSettings$: Observable<string> = this.carouselService.getChangedState().pipe(
+      tap(state => {
+        // this.update();
+        // this.carouselService.sendChanges();
+      })
+    );
+
+    const autoplayMerge$: Observable<string> = merge(initializedCarousel$, changedSettings$);
+    this.autoplaySubscription = autoplayMerge$.subscribe(
+      () => {}
+    );
+  }
 }
