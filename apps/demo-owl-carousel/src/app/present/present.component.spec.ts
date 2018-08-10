9a357a1a638838838b91c7e4fe8d49910b4b5589
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-10 11:55:55 +0300

    add PresentComponent to project=demo-owl-carousel

---
+++ apps/demo-owl-carousel/src/app/present/present.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PresentComponent } from './present.component';
+
+describe('PresentComponent', () => {
+  let component: PresentComponent;
+  let fixture: ComponentFixture<PresentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PresentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PresentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/demo-owl-carousel/src/app/present/present.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { CarouselData } from '../app.component';
+
+@Component({
+  selector: 'owl-carousel-libdemo-present',
+  templateUrl: './present.component.html',
+  styleUrls: ['./present.component.css']
+})
+export class PresentComponent implements OnInit {
+  carouselData: CarouselData[] = [
+    { text: 'Slide 1', dataMerge: 2, width: 300, dotContent: 'text1'},
+    { text: 'Slide 2', dataMerge: 1, width: 500, dotContent: 'text2'},
+    { text: 'Slide 3', dataMerge: 3, dotContent: 'text3'},
+    { text: 'Slide 4', width: 450, dotContent: 'text4'},
+    { text: 'Slide 5', dataMerge: 2, dotContent: 'text5'},
+    // { text: 'Slide 6', dotContent: 'text5'},
+    // { text: 'Slide 7', dotContent: 'text5'},
+    // { text: 'Slide 8', dotContent: 'text5'},
+    // { text: 'Slide 9', dotContent: 'text5'},
+    // { text: 'Slide 10', dotContent: 'text5'},
+  ];
+  title = 'owl-carousel-libdemo';
+  owlNext = '&rarr;';
+  owlPrev = '&larr;';
+
+
+  customOptions: any = {
+    // autoWidth: true,
+    loop: true,
+    // items: '10',
+    // margin: 10,
+    // slideBy: 'page',
+    // merge: true,
+    // autoplay: true,
+    autoplayTimeout: 5000,
+    // autoplayHoverPause: true,
+		autoplaySpeed: 4000,
+    dotsSpeed: 500,
+    // dots: false,
+    // dotsData: true,
+    // mouseDrag: false,
+    // touchDrag: false,
+    // pullDrag: false,
+    smartSpeed: 400,
+    // fluidSpeed: 499,
+    dragEndSpeed: 350,
+    // dotsEach: 4,
+    // center: true,
+    // rewind: true,
+    // rtl: true,
+    // startPosition: 1,
+    // navText: [ '<i class=fa-chevron-left>left</i>', '<i class=fa-chevron-right>right</i>' ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    },
+    // stagePadding: 40,
+    nav: true
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
