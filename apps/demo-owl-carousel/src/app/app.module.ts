1f6e5c2eda16ef3ba3b6c2dfc9b8e3d0167d8898
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-10 11:53:13 +0300

    add HomeModule to project=demo-owl-carousel

--- apps/demo-owl-carousel/src/app/app.module.ts
+++ apps/demo-owl-carousel/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { HomeModule } from './home/home.module';
+import { PresentModule } from './present/present.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot(), CarouselModule],
+  imports: [BrowserModule, NxModule.forRoot(), HomeModule, PresentModule],
   providers: [],
   bootstrap: [AppComponent]
 })
