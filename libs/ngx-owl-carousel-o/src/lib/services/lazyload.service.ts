baebef8455254dd5359038b725d34c6a40aff867
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-13 20:20:44 +0300

    add prop lazyLoadSubscription and spyDataStreams() to LazyLoadService

--- libs/ngx-owl-carousel-o/src/lib/services/lazyload.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/lazyload.service.ts
@@ -1,7 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Subscription, Observable, merge } from 'rxjs';
+import { CarouselService } from './carousel.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LazyLoadService {
+/**
+   * Subscrioption to merge Observable  from CarouselService
+   */
+  lazyLoadSubscription: Subscription;
 
-  constructor() { }
+  constructor(private carouselService: CarouselService) { }
+
+  /**
+   * Defines Observables which service must observe
+   */
+  spyDataStreams() {
+    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState().pipe(
+      tap(() => {
+
+      })
+    );
+
+    const changeSettings$: Observable<any> = this.carouselService.getChangeState().pipe(
+      tap(data => {
+      })
+    );
+
+    const resizedCarousel$: Observable<string> = this.carouselService.getResizedState().pipe();
+
+
+    const lazyLoadMerge$: Observable<string | any> = merge(initializedCarousel$, changeSettings$, resizedCarousel$);
+    this.lazyLoadSubscription = lazyLoadMerge$.subscribe(
+      () => {}
+    );
+  }
 }
