9c77a263d6f69cef1396b7a062c7900bcc624d93
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-14 19:00:49 +0300

    add spyDataStreams() and animateSubscription to AnimateService

--- libs/ngx-owl-carousel-o/src/lib/services/animate.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/animate.service.ts
@@ -1,7 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subscription, Observable, merge } from 'rxjs';
+import { CarouselService } from './carousel.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class AnimateService {
+export class AnimateService implements OnDestroy{
+  /**
+   * Subscrioption to merge Observable  from CarouselService
+   */
+  animateSubscription: Subscription;
 
-  constructor() { }
+  constructor(private carouselService: CarouselService) { }
+
+  ngOnDestroy() {
+    this.animateSubscription.unsubscribe();
+  }
+
+  /**
+   * Defines Observables which service must observe
+   */
+  spyDataStreams() {
+    const changeSettings$: Observable<any> = this.carouselService.getChangeState();
+
+    const translatedCarousel$: Observable<string> = this.carouselService.getTranslatedState();
+
+    const animateMerge$: Observable<string | any> = merge(changeSettings$).pipe();
+    this.animateSubscription = animateMerge$.subscribe(
+      () => {}
+    );
+  }
 }
