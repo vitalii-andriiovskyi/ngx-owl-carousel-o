af07acb3f296f52e0baa869babde6afe8cffb90f
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-14 13:50:34 +0300

    provide CarouselService into LazyLoadService

--- libs/ngx-owl-carousel-o/src/lib/services/lazyload.service.spec.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/lazyload.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { LazyLoadService } from './lazyload.service';
+import { CarouselService } from './carousel.service';
 
 describe('LazyloadService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LazyLoadService]
+      providers: [LazyLoadService, CarouselService]
     });
   });
 
