e16cc5b42eb203782ab7b52172aafd9904fa16f2
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-16 20:52:30 +0300

    update spyDataStreams(); AutoHeightService

--- libs/ngx-owl-carousel-o/src/lib/services/autoheight.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/autoheight.service.ts
@@ -20,10 +20,31 @@ export class AutoHeightService implements OnDestroy{
    * Defines Observables which service must observe
    */
   spyDataStreams() {
-    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState();
-    const changedSettings$: Observable<any> = this.carouselService.getChangedState();
+    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState().pipe(
+      tap(data => {
+        if (this.carouselService.settings.autoHeight) {
+          // this.update();
+        } else {
+          this.carouselService.slidesData.forEach(slide => slide.heightState = 'full');
+        }
+      })
+    );
 
-    const refreshedCarousel$: Observable<string> = this.carouselService.getRefreshedState();
+    const changedSettings$: Observable<any> = this.carouselService.getChangedState().pipe(
+      tap(data => {
+        if (this.carouselService.settings.autoHeight && data.property.name === 'position'){
+					// this.update();
+				}
+      })
+    );
+
+    const refreshedCarousel$: Observable<string> = this.carouselService.getRefreshedState().pipe(
+      tap(data => {
+        if (this.carouselService.settings.autoHeight) {
+          // this.update();
+        }
+      })
+    );
 
     const autoHeight$: Observable<string | any> = merge(initializedCarousel$, changedSettings$, refreshedCarousel$);
     this.autoHeightSubscription = autoHeight$.subscribe(
