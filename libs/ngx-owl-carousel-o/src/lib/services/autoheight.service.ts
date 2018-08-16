8172de85283a7b0193431f0535e9686ff6697a9a
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-16 19:56:56 +0300

    add autoHeightSubscription, spyDataStreams(); AutoHeightService

--- libs/ngx-owl-carousel-o/src/lib/services/autoheight.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/autoheight.service.ts
@@ -1,7 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subscription, Observable, merge } from 'rxjs';
+import { CarouselService } from './carousel.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class AutoHeightService {
+export class AutoHeightService implements OnDestroy{
+  /**
+   * Subscrioption to merge Observable  from CarouselService
+   */
+  autoHeightSubscription: Subscription;
+  constructor(private carouselService: CarouselService) {
+    this.spyDataStreams();
+  }
 
-  constructor() { }
+  ngOnDestroy() {
+    this.autoHeightSubscription.unsubscribe();
+  }
+  /**
+   * Defines Observables which service must observe
+   */
+  spyDataStreams() {
+    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState();
+    const changedSettings$: Observable<any> = this.carouselService.getChangedState();
+
+    const refreshedCarousel$: Observable<string> = this.carouselService.getRefreshedState();
+
+    const autoHeight$: Observable<string | any> = merge(initializedCarousel$, changedSettings$, refreshedCarousel$);
+    this.autoHeightSubscription = autoHeight$.subscribe(
+      () => {}
+    );
+  }
+
+
 }
