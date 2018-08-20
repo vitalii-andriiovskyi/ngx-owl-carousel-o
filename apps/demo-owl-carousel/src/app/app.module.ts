32082db3c7cab9873af3114a13dd9041ca7be772
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-20 13:27:30 +0300

    add routes to AppModule

--- apps/demo-owl-carousel/src/app/app.module.ts
+++ apps/demo-owl-carousel/src/app/app.module.ts
@@ -6,10 +6,26 @@ import { NxModule } from '@nrwl/nx';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HomeModule } from './home/home.module';
 import { PresentModule } from './present/present.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  // { path: 'home', component: CrisisListComponent },
+  // { path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full'
+  // }
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot(), HomeModule, PresentModule, CarouselModule],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    HomeModule,
+    PresentModule,
+    CarouselModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
