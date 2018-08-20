727323dd8a74197c6db286ab9fdf3790742ed84b
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-20 15:02:18 +0300

    add hashSubscription and spyDataStreams(); HashService

--- libs/ngx-owl-carousel-o/src/lib/services/hash.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/hash.service.ts
@@ -1,7 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subscription, Observable, merge } from 'rxjs';
+import { CarouselService } from './carousel.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class HashService {
+export class HashService implements OnDestroy {
+  /**
+   * Subscription to merge Observable from CarouselService
+   */
+  hashSubscription: Subscription;
 
-  constructor() { }
+  constructor(private carouselService: CarouselService) {
+    this.spyDataStreams();
+  }
+
+  ngOnDestroy() {
+    this.hashSubscription.unsubscribe();
+  }
+  /**
+   * Defines Observables which service must observe
+   */
+  spyDataStreams() {
+    const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState().pipe(
+      tap(data => {})
+    );
+
+    const changedSettings$: Observable<any> = this.carouselService.getChangedState().pipe(
+      tap(data => {})
+    );
+
+    const hashFragment$: Observable<string | any> = merge(initializedCarousel$, changedSettings$);
+    this.hashSubscription = hashFragment$.subscribe(
+      () => {}
+    );
+  }
 }
