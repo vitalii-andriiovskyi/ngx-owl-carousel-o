20a6d2b9bf58a5ef7d1914a8529da8439c44c35a
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-20 13:28:13 +0300

    add routes to HomeModule

--- apps/demo-owl-carousel/src/app/home/home.module.ts
+++ apps/demo-owl-carousel/src/app/home/home.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule, Routes } from '@angular/router';
+import { SubHomeComponent } from './subhome/subhome.component';
+
+const appRoutes: Routes = [
+  { path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'subhome', component: SubHomeComponent}
+    ] },
+];
 
 @NgModule({
   imports: [
-    CommonModule, CarouselModule
+    CommonModule, CarouselModule, RouterModule.forChild(appRoutes)
   ],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SubHomeComponent],
   exports: [ HomeComponent]
 })
 export class HomeModule { }
