8b9296d64995c7bbcf192033321cbaad3d36087c
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-21 18:53:47 +0300

    add RouterModule and const appRoutes to PresentModule

--- apps/demo-owl-carousel/src/app/present/present.module.ts
+++ apps/demo-owl-carousel/src/app/present/present.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentComponent } from './present.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'present', component: PresentComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule, CarouselModule
+    CommonModule, CarouselModule, RouterModule.forChild(appRoutes)
   ],
   declarations: [PresentComponent],
   exports: [ PresentComponent ]
