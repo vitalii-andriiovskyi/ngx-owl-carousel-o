c482fd54bc6bc4bb787431fbf90e5d4cbea5c72f
Vitalii Andriiovskyi <domovik0712@gmail.com>
2018-08-21 12:24:56 +0300

    correct behavior for startPosition === 'URLHash'; HashService

--- libs/ngx-owl-carousel-o/src/lib/services/hash.service.ts
+++ libs/ngx-owl-carousel-o/src/lib/services/hash.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Subscription, Observable, merge } from 'rxjs';
 import { CarouselService } from './carousel.service';
-import { tap } from 'rxjs/operators';
+import { tap, skip } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable()
@@ -10,11 +10,6 @@ export class HashService implements OnDestroy {
    * Subscription to merge Observable from CarouselService
    */
   hashSubscription: Subscription;
-
-  /**
-   * Subscription to ActivatedRoute.fragment
-   */
-  routeSubscription: Subscription;
 
   /**
    * Current url fragment (hash)
@@ -29,7 +24,6 @@ export class HashService implements OnDestroy {
 
   ngOnDestroy() {
     this.hashSubscription.unsubscribe();
-    this.routeSubscription.unsubscribe();
   }
 
   /**
@@ -37,11 +31,7 @@ export class HashService implements OnDestroy {
    */
   spyDataStreams() {
     const initializedCarousel$: Observable<string> = this.carouselService.getInitializedState().pipe(
-      tap(data => {
-        if (this.carouselService.settings.startPosition === 'URLHash') {
-					this.listenToRoute();
-				}
-      })
+      tap(() => this.listenToRoute() )
     );
 
     const changedSettings$: Observable<any> = this.carouselService.getChangedState().pipe(
@@ -82,11 +72,15 @@ export class HashService implements OnDestroy {
    * Initiate listening to ActivatedRoute.fragment
    */
   listenToRoute() {
-    this.routeSubscription = this.route.fragment.subscribe(
-      fragment => {
-        this.currentHashFragment = fragment;
-        this.rewind(fragment);
-      }
-    )
+    const count = this.carouselService.settings.startPosition === 'URLHash' ? 0 : 2;
+    this.route.fragment.pipe(
+        skip(count)
+      )
+      .subscribe(
+        fragment => {
+          this.currentHashFragment = fragment;
+          this.rewind(fragment);
+        }
+      )
   }
 }
