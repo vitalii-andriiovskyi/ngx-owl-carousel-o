a10a71c2027ebda45c234549b71f3faf7ff1f85d
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-02-07 17:56:42 +0200

    refactor(CarouselModule): remove RouterModule from imports

--- libs/ngx-owl-carousel-o/src/lib/carousel/carousel.module.ts
+++ libs/ngx-owl-carousel-o/src/lib/carousel/carousel.module.ts
@@ -27,7 +27,8 @@ const routes: Routes = [];
   imports: [
     CommonModule,
     // BrowserAnimationsModule, // there's an issue with this import while using lazy loading of module consuming this library. I don't remove it because it could be needed during future enhancement of this lib.
-    RouterModule.forChild(routes)],
+    // RouterModule.forChild(routes)
+  ],
   declarations: [CarouselComponent, CarouselSlideDirective, StageComponent, OwlRouterLinkDirective, OwlRouterLinkWithHrefDirective],
   exports: [CarouselComponent, CarouselSlideDirective, OwlRouterLinkDirective, OwlRouterLinkWithHrefDirective],
   providers: [WINDOW_PROVIDERS, ResizeService, DOCUMENT_PROVIDERS, OwlLogger]
