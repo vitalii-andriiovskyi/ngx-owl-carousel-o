059d221f642986a97e6754c5ecf5165debecaf78
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-05-07 11:33:11 +0300

    feat: DoubledCarouselModule and DoubledCarouselComponent

---
+++ apps/demo-owl-carousel/src/app/doubled-carousel/doubled-carousel/doubled-carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'owl-carousel-libdemo-doubled-carousel',
+  templateUrl: './doubled-carousel.component.html',
+  styleUrls: ['./doubled-carousel.component.scss']
+})
+export class DoubledCarouselComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
