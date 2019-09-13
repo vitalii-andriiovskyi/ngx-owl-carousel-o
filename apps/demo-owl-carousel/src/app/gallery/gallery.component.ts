e498c6df658785887765c559d6d5db1548bc87b7
Vitalii Andriiovskyi <domovik0712@gmail.com>
2019-09-13 10:56:05 +0300

    feat: GalleryComponent

---
+++ apps/demo-owl-carousel/src/app/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'owl-carousel-libdemo-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.sass']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
